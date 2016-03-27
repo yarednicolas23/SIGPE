cdd75751ffdde07d7f3ec52fa0c9677627044bb2
yarednicolas23 <yarednicolas@gmail.com>
2016-03-27 12:06:54 -0500

    add

--- build/generated-sources/ap-source-output/Entities/Auditoria_.java
+++ build/generated-sources/ap-source-output/Entities/Auditoria_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-03-27T12:02:21")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-03-27T12:05:39")
 @StaticMetamodel(Auditoria.class)
 public class Auditoria_ { 
 
