4819a6a4d812dbb0c370fcc6151f4b9be6fb877a
yarednicolas23 <yarednicolas@gmail.com>
2016-03-27 17:16:20 -0500

    Edit

--- build/generated-sources/ap-source-output/Entities/Empresa_.java
+++ build/generated-sources/ap-source-output/Entities/Empresa_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-03-27T12:07:09")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-03-27T12:09:48")
 @StaticMetamodel(Empresa.class)
 public class Empresa_ { 
 
