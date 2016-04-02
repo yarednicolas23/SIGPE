1c981537a385062258106b32856cefd2883125b0
yarednicolas23 <yarednicolas@gmail.com>
2016-04-02 18:10:22 -0500

    Add controler lenguages

--- build/generated-sources/ap-source-output/Entities/Empresa_.java
+++ build/generated-sources/ap-source-output/Entities/Empresa_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-04-02T11:45:24")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-04-02T18:05:27")
 @StaticMetamodel(Empresa.class)
 public class Empresa_ { 
 
